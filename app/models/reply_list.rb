50bb653ffeea76a45668209c793570e069375b5a
nagatech <cheesecakebot@yahoo.co.jp>
2017-09-29 16:25:19 +0900

    Revert
    This reverts commit d4d1c60550093240e49a597900f5d2df123d2eb3.

--- app/models/reply_list.rb
+++
@@ -1,2 +0,0 @@
-class ReplyList < ApplicationRecord
-end
--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,3 @@
 Rails.application.routes.draw do
-  get 'reply_lists/index'
-
-  get 'reply_lists/add'
-
-  get 'reply_lists/edit'
-
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
