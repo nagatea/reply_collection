210f8000a19ac51a1fc3ba2252a20bbfeb26e2d4
nagatech <cheesecakebot@yahoo.co.jp>
2017-10-01 02:55:04 +0900

    とりあえず動くところまで

--- app/controllers/reply_lists_controller.rb
+++ app/controllers/reply_lists_controller.rb
@@ -1,13 +1,38 @@
 class ReplyListsController < ApplicationController
   def index
+    @list = ReplyList.all
   end
 
   def add
+    if request.post? then
+      ReplyList.create(
+        params.require(:reply_list).permit(:reply, :count, :created_time)
+      )
+      goback
+    else
+      @list = ReplyList.new
+    end
   end
 
   def edit
+    @list = ReplyList.find(params[:id])
+    if request.patch? then
+      @list.update(list_params)
+      goback
+    end
   end
 
   def detail
+    @list = ReplyList.find(params[:id])
+  end
+
+  private
+  def list_params
+    params.require(:reply_list).permit(:reply, :count, :created_time)
+  end
+
+  private
+  def goback
+    redirect_to '/reply_lists'
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,10 @@
 Rails.application.routes.draw do
+  get 'reply_lists', to: 'reply_lists#index'
+
   get 'reply_lists/index'
 
   get 'reply_lists/add'
+  post 'reply_lists/add'
 
   get 'reply_lists/edit'
 
